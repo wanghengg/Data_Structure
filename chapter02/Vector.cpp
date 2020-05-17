bee470575e5cf09ebaf6eedccc9b647904ce1868
wangheng <wh9697yyk@163.com>
2020-05-17 22:56:44 +0800

    添加Vector类模板的声明和实现

--- chapter02/Vector.cpp
+++ chapter02/Vector.cpp
@@ -2,7 +2,19 @@
 // Created by wangheng on 2020/5/13.
 //
 
-#include "vector.h"
+#include "Vector.h"
+#include "swap.h"
+#include "binSearch.h"
+#include "fibSearch.h"
+#include <cstdlib>
+
+template<typename T>
+void Vector<T>::copyFrom(const T* A, Rank lo, Rank hi) {
+    _elem = new T[_capacity = 2 * (hi - lo)];
+    _size = 0;
+    while (lo < hi)
+        _elem[_size++] = A[lo++];
+}
 
 template<typename T>
 Rank Vector<T>::max(Rank lo, Rank hi) {
@@ -29,6 +41,22 @@ void Vector<T>::shrink() {
     for (int i = 0; i < _size; i++)
         _elem[i] = oldElem[i];
     delete [] oldElem;
+}
+
+template<typename T>
+void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
+    T* S = new T[hi - lo];
+    int i = lo, j = mi;
+    int k = 0;
+    for (; i < mi && j < hi;)
+        S[k++] = (_elem[i] < _elem[j]) ? _elem[i++] : _elem[j++];
+    for (; i < mi;)
+        S[k++] = _elem[i++];
+    for (; j < hi;)
+        S[k++] = _elem[j++];
+    for (int m = 0; m < hi - lo; m++)
+        _elem[lo + m] = S[m];
+    delete [] S;
 }
 
 template<typename T>
@@ -70,7 +98,12 @@ Rank Vector<T>::find(const T &e, Rank lo, Rank hi) const {
 }
 
 template<typename T>
-int Vector<T>::depulicate() {
+Rank Vector<T>::find(const T &e) const {
+    return find(e, 0, _size);
+}
+
+template<typename T>
+int Vector<T>::deduplicate() {
     int oldSize = _size;
     Rank i = 1;
     while (i < _size) {
@@ -78,6 +111,69 @@ int Vector<T>::depulicate() {
             i++;
         else remove(i);
     }
-
     return oldSize - _size;
 }
+
+// 遍历向量
+template<typename T>
+void Vector<T>::traverse(void (*visit)(T &)) {
+    for (int i = 0; i < _size; i++)
+        visit(_elem[i]);
+}
+
+template<typename T>
+int Vector<T>::disordered() const {
+    int n = 0;
+    for (int i = 1; i < _size; i++)
+        n += (_elem[i-1] > _elem[i]);
+    return n;
+}
+
+template<typename T>
+int Vector<T>::uniquify() {
+    int i = 0, j = 0;
+    while (++j < _size) {
+        if (_elem[i] != _elem[j])
+            _elem[++i] = _elem[j];
+    }
+    _size = ++i;
+    shrink();
+    return j - i;
+}
+
+template<typename T>
+void Vector<T>::unsort(Rank lo, Rank hi) {
+    T* V = _elem + lo;
+    for (Rank i = hi - lo; i > 0; i--)
+        swap(V[i-1], V[std::rand() / i]);
+}
+
+template<typename T>
+Rank Vector<T>::search(const T &e, Rank lo, Rank hi) const {
+    return (rand() % 2) ? binSearch(_elem, e, lo, hi) : fibSearch(_elem, e, lo, hi);
+}
+
+template<typename T>
+void Vector<T>::bubbleSort(Rank lo, Rank hi) {  // 起泡排序是稳定的
+    Rank last = lo; // 记录最后一次交换逆序对的位置
+    // 后面是顺序的，所以直接跳转到last位置，后面不用扫描
+    for(bool sorted = false; sorted = !sorted; hi = last) {
+        for (int i = lo + 1; i < hi; i++) {
+            if (_elem[i] < _elem[i - 1]) {
+                swap(_elem[i], _elem[i - 1]);
+                last = i;
+                sorted = false;
+            }
+        }
+    }
+}
+
+template<typename T>
+void Vector<T>::mergeSort(Rank lo, Rank hi) {
+    if (hi - lo < 2) return;
+    Rank mi = (lo + hi) >> 1;
+    mergeSort(lo, mi);
+    mergeSort(mi, hi);
+    merge(lo, mi, hi);
+}
+
--- chapter02/Vector.h
+++ chapter02/Vector.h
@@ -2,8 +2,8 @@
 // Created by wangheng on 2020/5/13.
 //
 
-#ifndef DATA_STRUCTURE_VECTOR_H
-#define DATA_STRUCTURE_VECTOR_H
+#ifndef TEST_VECTOR_H
+#define TEST_VECTOR_H
 
 typedef int Rank ;
 #define DEFAULT_CAPACITY 3
@@ -14,26 +14,27 @@ private:
     int _capacity;
     T* _elem;
 protected:
-    void copyFrom(T const *A, Rank lo, Rank hi)   // 赋值数组区间[lo, hi)
-    {
-        _elem = new T[_capacity = (hi - lo) * 2];
-        _size = 0;
-        while (lo < hi) {
-            _elem[_size++] = A[lo++];
-        }
-    }
+    void copyFrom(const T* A, Rank lo, Rank hi);   // 赋值数组区间[lo, hi)
     Rank max(Rank lo, Rank hi);
     void expand();  // 空间不足时扩充
     void shrink();  // 装填因子过小时压缩
+    void merge(Rank lo, Rank mi, Rank hi);
 public:
     // 构造函数
     Vector(int c = DEFAULT_CAPACITY, int s = 0, T v = 0) : _capacity(c) {
         _elem = new T[_capacity];
         for (_size = 0; _size < s; _elem[_size++] = v);
     }
-    Vector(T const * A, Rank n) { copyFrom(A, 0, n); }
-    Vector(T const * A, Rank lo, Rank hi) { copyFrom(A, lo, hi); }
+    Vector(const T* A, Rank n) { copyFrom(A, 0, n); }
+    Vector(const T* A, Rank lo, Rank hi) { copyFrom(A, lo, hi); }
     Vector(const Vector<T>& vec) { copyFrom(vec._elem, 0, vec._size); }
+    // 定义拷贝赋值运算符
+    Vector<T>& operator=(const Vector<T>& vec) {
+        if (_elem)
+            delete [] _elem;
+        copyFrom(vec._elem, 0, vec._size);
+        return *this;
+    }
     Vector(const Vector<T>& vec, Rank lo, Rank hi) { copyFrom(vec._elem, lo, hi); }
     // 析构函数
     ~Vector() { delete [] _elem; }
@@ -44,9 +45,22 @@ public:
     Rank insert(Rank r, T const &e);
     int remove(Rank lo, Rank hi);
     T remove(Rank r);
-    Rank find(T const & e, Rank lo, Rank hi) const ;
-    int depulicate();
-
+    Rank find(T const & e, Rank lo, Rank hi) const ;    // 无序向量区间查找
+    Rank find(T const & e) const; // 无序向量整体查找
+    int deduplicate();  // 无序向量唯一化
+    void traverse(void (*visit)(T&));
+    int disordered() const ;    // 计算逆序对
+    int uniquify();     // 有序向量唯一化
+    void unsort(Rank lo, Rank hi);  // 等概率随机置乱区间[lo, hi)
+    Rank search(T const& e, Rank lo, Rank hi) const;    // 有序区间向量区间查找
+    Rank search(T const& e) const  // 有序向量整体查找
+    {
+        return (0 >= _size) ? -1 : search(e, 0, _size);
+    }
+    void bubbleSort(Rank lo, Rank hi);  // 冒泡排序
+    void bubbleSort() { bubbleSort(0, _size); }
+    void mergeSort(Rank lo, Rank hi);   // 归并排序
+    void mergeSort() { mergeSort(0, _size); }
 };
 
-#endif //DATA_STRUCTURE_VECTOR_H
+#endif //TEST_VECTOR_H
