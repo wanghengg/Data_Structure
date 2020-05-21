314cde1afa6ba4e00e417935fc5c89cdb4e62681
wangheng <wh9697yyk@163.com>
2020-05-21 12:13:33 +0800

    添加了chapter02/main.cpp

--- chapter02/main.cpp
+++ chapter02/main.cpp
@@ -2,17 +2,15 @@
 // Created by wangheng on 2020/5/16.
 //
 
-#include "Vector.h"
-#include "Vector.cpp"
 #include <iostream>
+#include "bitMap.h"
 
 int main() {
-    int a[] = {3,1,2,9,5,3,9,1,0,8,9,1,7,34,434,21,34,5,99};
-    int length = sizeof(a)/sizeof(a[0]);
-    Vector<int> vec(a, length);
-    vec.mergeSort(1, 9);
-    for (int i = 0; i < length; i++)
-        std::cout << vec[i] << std::endl;
+    BitMap bitmap;
+    bitmap.set(79999);
+    std::cout << std::boolalpha << bitmap.get(79999) << std::endl;
+    bitmap.set(3);
+    std::cout << bitmap.get(3) << std::endl;
 
     return 0;
 }
