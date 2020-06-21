d884335b13a045e4173975226d261adaa6431a1c
wangheng <wh9697yyk@163.com>
2020-06-21 15:53:25 +0800

    update

--- chapter02/main.cpp
+++ chapter02/main.cpp
@@ -1,16 +1,12 @@
-//
-// Created by wangheng on 2020/5/16.
-//
-
 #include <iostream>
-#include "bitMap.h"
+#include "Vector.h"
 
 int main() {
-    BitMap bitmap;
-    bitmap.set(79999);
-    std::cout << std::boolalpha << bitmap.get(79999) << std::endl;
-    bitmap.set(3);
-    std::cout << bitmap.get(3) << std::endl;
+    Vector<int> ivec;
+    ivec.insert(3);
+    ivec.insert(9);
+    std::cout << ivec.size() << std::endl;
+    std::cout << ivec.search(3) << std::endl;
 
     return 0;
 }
